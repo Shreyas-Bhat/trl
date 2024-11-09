dfead82b6b341e02156d069ce8ee050f82da9055
Shreyas Bhat <56354373+Shreyas-Bhat@users.noreply.github.com>
2024-11-09 19:06:47 +0530

    "Fixes"

--- trl/trainer/rloo_trainer.py
+++ trl/trainer/rloo_trainer.py
@@ -731,6 +731,7 @@ class RLOOTrainer(Trainer):
                     postprocessed_query_response = torch.cat((query, postprocessed_response), 1)
                     llm_output = forward(self.llm_decision_maker, postprocessed_query_response, processing_class.pad_token_id)
                     llm_scores = llm_output
+                    ground_truth = batch["labels"].to(response.device)
                     _, score, _ = get_reward(
                         self.reward_model, 
                         postprocessed_query_response, 
